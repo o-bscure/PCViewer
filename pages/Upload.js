d475b4d7203907b06fd8dca2007a751d96c96ef2
o-bscure <anthonyoreshkov@gmail.com>
2021-08-24 13:52:25 -0700

    uploads and view links

---
+++ pages/Upload.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function Upload() {
+  return (
+      <div className="grid grid-cols-1 w-screen h-screen bg-green-300 place-items-center">
+        <div>upload imgs here</div>
+      </div>
+  )
+}
