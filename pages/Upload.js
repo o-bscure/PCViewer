2a0e234117d3af7b708a690766ccd2c26242b8ff
o-bscure <anthonyoreshkov@gmail.com>
2021-08-25 14:06:00 -0700

    run/tank column mirroring the image select column

--- pages/Upload.js
+++ pages/Upload.js
@@ -6,36 +6,52 @@ export default class Upload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            files: {
-                "0": {
-                    img: null,
-                    name: null,
-                    run: null,
-                    tank: null,
-                },
-            }
+            run: "",
+            files: {}
         }
 
+        this.handleRunSelect = this.handleRunSelect.bind(this);
+        this.handleTankSelect = this.handleTankSelect.bind(this)
         this.handleFileSelect = this.handleFileSelect.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleFileSelect(event, filenum) {
+    handleTankSelect(event, filenumber) {
+        event.preventDefault()
+        this.setState(prevState => ({
+            ...prevState,
+            files: {
+                ...prevState.files,
+                [filenumber]: {
+                    ...prevState.files[filenumber],
+                    tank: event.target.value
+                }
+            }
+        }))
+    }
+
+    handleRunSelect(event) {
+        event.preventDefault()
+        this.setState(prevState => ({
+            run: event.target.value 
+        }))
+    }
+
+    handleFileSelect(event, filenumber) {
         event.preventDefault()
         const f = event.target.files[0]
         const idExists = typeof this.state.files[0] != "undefined";
-        console.log(Object.keys(event.target.files))
-        console.log(event.target.filenum)
-        this.setState({
+        this.setState(prevState => ({
+            ...prevState,
             files: {
-                0: { //TODO: dynamic file number state updating
+                ...prevState.files,
+                [filenumber]: { //TODO: dynamic file number state updating
+                    ...prevState.files[filenumber],
                     img: URL.createObjectURL(f),
                     name: f.name,
-                    run: null,
-                    tank: null,
                 }
             }
-        })
+        }))
     }
 
     handleSubmit(event) {
@@ -43,27 +59,44 @@ export default class Upload extends React.Component {
 
     render() {
         console.log(this.state)
-        var rows = [<input type="file" key={0} fileNum="0" accept="image/*" onChange={this.handleFileSelect.bind(this)} className="flex flex-initial"/>]
-        /*
-        for (let i=1; i<this.state.filenames.length+1; i++) {
-            rows.push(<input type="file" key={i} id="img" name="img" accept="imgage/*" onChange={this.handleFileSelect} className="flex flex-initial"/>)
+
+        var filerows = [];
+        for (let i=0; i<Object.keys(this.state.files).length+1; i++) {
+            filerows.push(<input type="file" key={i} accept="image/*" onChange={(e) => this.handleFileSelect(e, i)} className="flex flex-initial"/>)
         }
-        */
+
+        var tankrows = []
+        for (let i=0; i<Object.keys(this.state.files).length; i++) {
+            tankrows.push(
+                <div key={i} className="flex flex-rows-1 pt-1 pb-0.5 place-content-center">
+                    <label className="flex flex-initial">Tank No. </label>
+                    <input type="number" onChange={(e) => this.handleTankSelect(e, i)} className="flex flex-initial w-14"/>
+                </div>
+            )
+        }
+
+        let imgs = Object.keys(this.state.files).map((imgkey) => <img key={imgkey} src={this.state.files[imgkey].img} />)
+        /* console.log(imgs) */
 
         return (
-          <div className="grid grid-cols-1 w-screen h-screen bg-green-300">
+          <div className="grid grid-cols-1 w-screen h-screen bg-gray-200">
             <form onSubmit={this.handleSubmit} className="grid grid-cols-3 w-full h-full">
-                <div className="flex flex-col flex-nowrap">
+                <div className="flex flex-col flex-nowrap gap-y-5">
+                    <div className="flex flex-rows-1 place-content-center m-5 mt-8">
+                        <label className="flex flex-inital text-2xl pr-4">Enter Run</label>
+                        <input type="text" id="run" value={this.state.run} onChange={(e) => this.handleRunSelect(e)} className="flex flex-initial w-20"/>
+                    </div>
+                    <div className="flex flex-col flex-grow gap-y-3 ">{tankrows}</div>
                 </div>
 
                 <div className="flex flex-col flex-nowrap gap-y-5">
-                    <label className="flex flex-inital m-5 justify-center text-2xl">Select PCV image</label>
-                    <div className="flex flex-col flex-grow gap-y-2">{rows}</div>
-                    <img src={this.state.files[0].img}/>
-                    <input type="submit" className="flex flex-inital"/>
+                    <label className="flex flex-inital m-5 mt-8 place-self-center text-2xl">Select PCV image</label>
+                    <div className="flex flex-col flex-grow gap-y-3">{filerows}</div>
+                    <input type="submit" className="flex flex-initial place-self-center justify-center m-3 w-40 h-20 rounded-t-md"/>
                 </div>
 
-                <div className="flex flex-col flex-nowrap">
+                <div className="flex flex-col flex-nowrap gap-y-5">
+                    <label className="flex flex-inital m-5 mt-8 place-self-center text-2xl">Results</label>
                 </div>
             </form>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-        <div className="grid grid-rows-2 h-screen w-screen bg-gray-300 place-items-center">
+        <div className="grid grid-rows-2 h-screen w-screen bg-gray-200 place-items-center">
             <h1 className="font-sans font-semibold text-5xl">PCV VIEWER</h1>
-            <div className="grid grid-cols-4 grid-rows-2 bg-gray-300 w-full h-full place-items-center">
+            <div className="grid grid-cols-4 grid-rows-2 bg-gray-200 w-full h-full place-items-center">
                 <span/>
                 <Link href="/upload">
                     <button className="rounded-lg border-2 border-red-600 p-5 w-40 text-3xl"><p>Upload</p></button>
