337b37608f053e5e0e0c48fdf603655ca6fe23f5
o-bscure <anthonyoreshkov@gmail.com>
2021-08-25 21:26:56 -0700

    full tank column functionality

--- pages/Upload.js
+++ pages/Upload.js
@@ -1,23 +1,61 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { nonUnique } from '../lib/nonUnique.js'
 
 export default class Upload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             run: "",
-            files: {}
+            files: {},
+            bad_tanks: []
         }
 
         this.handleRunSelect = this.handleRunSelect.bind(this);
         this.handleTankSelect = this.handleTankSelect.bind(this)
         this.handleFileSelect = this.handleFileSelect.bind(this);
+        this.setTankNumbers = this.setTankNumbers.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setTankNumbers(e) {
+        e.preventDefault()
+        const file_keys = Object.keys(this.state.files)
+        for (let i=0; i<file_keys.length; i++) {
+            this.setState(prevState => ({
+                ...prevState,
+                files: {
+                    ...prevState.files,
+                    [file_keys[i]]: {
+                        ...prevState.files[file_keys[i]],
+                        tank: String(i+1)
+                    }
+                }
+            }))
+        }
     }
 
     handleTankSelect(event, filenumber) {
         event.preventDefault()
+
+        var tanks= Object.keys(this.state.files)
+        .map((file_key) => {
+            if (file_key == filenumber) {
+                let x = this.state.files[file_key]
+                x.tank = event.target.value
+                return x
+            }
+            return this.state.files[file_key]
+        })
+        .reduce((tank_numbers, file) => {
+            if (typeof file.tank != "undefined") {
+                tank_numbers.push(file.tank)
+            }
+            return tank_numbers
+        }, [])
+        var bad_tanks = nonUnique(tanks)
+
         this.setState(prevState => ({
             ...prevState,
             files: {
@@ -26,7 +64,8 @@ export default class Upload extends React.Component {
                     ...prevState.files[filenumber],
                     tank: event.target.value
                 }
-            }
+            },
+            bad_tanks: bad_tanks
         }))
     }
 
@@ -45,7 +84,7 @@ export default class Upload extends React.Component {
             ...prevState,
             files: {
                 ...prevState.files,
-                [filenumber]: { //TODO: dynamic file number state updating
+                [filenumber]: { 
                     ...prevState.files[filenumber],
                     img: URL.createObjectURL(f),
                     name: f.name,
@@ -62,15 +101,20 @@ export default class Upload extends React.Component {
 
         var filerows = [];
         for (let i=0; i<Object.keys(this.state.files).length+1; i++) {
-            filerows.push(<input type="file" key={i} accept="image/*" onChange={(e) => this.handleFileSelect(e, i)} className="flex flex-initial"/>)
+            filerows.push(<input type="file" key={i} accept="image/*" onChange={(e) => this.handleFileSelect(e, i)} className="flex flex-initial h-10 "/>)
         }
 
         var tankrows = []
         for (let i=0; i<Object.keys(this.state.files).length; i++) {
+            if (typeof this.state.files[i].tank != "undefined" && this.state.bad_tanks.includes(this.state.files[i].tank)) {
+                var box_type = "flex flex-initial place-self-center w-10 h-7 bg-gray-200 border-2 border-red-600 rounded-md";
+            } else {
+                var box_type ="flex flex-initial place-self-center w-10 h-7 bg-gray-200 border-2 border-gray-100 rounded-md";
+            }
             tankrows.push(
-                <div key={i} className="flex flex-rows-1 pt-1 pb-0.5 place-content-center">
-                    <label className="flex flex-initial">Tank No. </label>
-                    <input type="number" onChange={(e) => this.handleTankSelect(e, i)} className="flex flex-initial w-14"/>
+                <div key={i} className="flex flex-rows-1 h-10 place-content-center">
+                    <label className="flex flex-initial place-items-center">Tank No. </label>
+                    <input type="number" value={this.state.files[i].tank} onChange={(e) => this.handleTankSelect(e, i)} className={box_type}/>
                 </div>
             )
         }
@@ -84,15 +128,17 @@ export default class Upload extends React.Component {
                 <div className="flex flex-col flex-nowrap gap-y-5">
                     <div className="flex flex-rows-1 place-content-center m-5 mt-8">
                         <label className="flex flex-inital text-2xl pr-4">Enter Run</label>
-                        <input type="text" id="run" value={this.state.run} onChange={(e) => this.handleRunSelect(e)} className="flex flex-initial w-20"/>
+                        <input type="text" id="run" value={this.state.run} onChange={(e) => this.handleRunSelect(e)} 
+                            className="flex flex-initial w-20 bg-gray-200 border-2 border-gray-700 rounded-lg"/>
                     </div>
-                    <div className="flex flex-col flex-grow gap-y-3 ">{tankrows}</div>
+                    <div className="flex flex-col flex-grow gap-y-5 ">{tankrows}</div>
+                    <button onClick={(e) => this.setTankNumbers(e)} className="flex flex-initial place-self-center bg-gray-100 m-3 p-6 rounded-t-md">Auto</button>
                 </div>
 
                 <div className="flex flex-col flex-nowrap gap-y-5">
                     <label className="flex flex-inital m-5 mt-8 place-self-center text-2xl">Select PCV image</label>
-                    <div className="flex flex-col flex-grow gap-y-3">{filerows}</div>
-                    <input type="submit" className="flex flex-initial place-self-center justify-center m-3 w-40 h-20 rounded-t-md"/>
+                    <div className="flex flex-col flex-grow gap-y-5 ">{filerows}</div>
+                    <input type="submit" className="flex flex-initial place-self-center justify-center m-3 w-40 h-20 rounded-t-md bg-gray-100"/>
                 </div>
 
                 <div className="flex flex-col flex-nowrap gap-y-5">
